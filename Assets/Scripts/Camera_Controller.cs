356c34fadce02cc09d63082462ff1ace853eef1e
Shruthi Pragna Oruganti <182078145+shruthipragnao-2@users.noreply.github.com>
2025-09-10 19:17:20 +0530

    Map_initial
    Base map with terrain, mountains and trees + lake

---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,11 @@
+namespace DevHub.Clash1
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Camera_Controller : MonoBehaviour
+    {
+
+    }
+}
